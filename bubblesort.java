a69e7dcf8f5598d9f5df29c751b9a84c6343ada6
Evgeny <74717794+Vasilshift@users.noreply.github.com>
2021-03-06 21:39:15 +0500

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,29 @@
+public class bubblesort {
+    public static void main(String[] args) {
+
+        String str1 = "xtraf";
+        System.out.println(str1);
+        byte[] array = str1.getBytes();
+        //System.out.println(array);
+
+        boolean isSorted = false;
+
+        while(!isSorted){
+
+            isSorted = true;
+            for (int i =1; i < array.length; i++){
+                if (array[i] < array[i-1]){
+                    int temp = array[i];
+                    array[i] = array[i-1];
+                    array[i-1] = (byte) temp;
+                    isSorted = false;
+                }
+
+            }
+            System.out.println(new String(array));
+
+        }
+
+    }
+
+}
